b8afb2b0313ae4f4846238c9e0bc81a5ed499863
Bell2U <495050731@qq.com>
2021-04-15 21:05:41 +0800

    importerror

--- dh/dirtest.py
+++ dh/dirtest.py
@@ -5,3 +5,31 @@
 
 
 # import lib.comms
+
+
+
+# another way of doing this
+# import sys, os
+# find the current executing directory of this py file
+# current_dir = sys.path[0]
+# print(current_dir)
+
+# find its parent
+# parent = os.path.join(current_dir, os.pardir)
+# print(parent)
+# parent = os.path.realpath(parent)
+# print(parent)
+
+# change the current directory to its parent, making module import availble
+# sys.path[0] = parent
+
+# test
+# from colours import Colour
+
+
+
+# lite edition
+# import sys, os
+# sys.path[0] = os.path.realpath(os.path.join(sys.path[0], os.pardir))
+
+# from colours import Colour
